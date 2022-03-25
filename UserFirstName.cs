923ad913c00ffaf8fe98a5672caf7226657ded45
murgendrag <murgendra67@gmail.com>
2022-03-25 23:57:48 +0530

    firstname checked

---
+++ UserFirstName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserFirstName
+    {
+
+            public static string FirstName = "^[A-Z]{1}[A-Z a-z]{15}$";
+            public bool Validate_First_Name(string First_Name)
+            {
+                return Regex.IsMatch(First_Name,FirstName);
+
+            }
+
+        }
+    }
