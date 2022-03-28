087347aed13677c47b9a639fd49cad1362bb75ea
murgendrag <murgendra67@gmail.com>
2022-03-28 23:59:56 +0530

    Password update

--- Password.cs
+++ Password.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace UserRegistrationProblem
 {
-    internal class Password
+    public class Password
     {
+        public static string Pass_Word = "^(?=.*[A-Za-z])(?=.*/d)[A-Za-z/d]{8,}$";
+        public static string Pass_Word2 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*/d)[a-zA-Z/d]{8,}$";
+        public bool Validate_Rule1(string passWord)
+        {
+            return Regex.IsMatch(Pass_Word, passWord);
+
+        }
+        public bool Validate_Rule2(string passWord2)
+        {
+            return Regex.IsMatch(Pass_Word2, passWord2);
+
+        }
     }
 }
