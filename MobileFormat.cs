509d586cd36635fa6a588af8e8d5c48ce1178314
murgendrag <murgendra67@gmail.com>
2022-03-28 23:12:13 +0530

    mobile number

---
+++ MobileFormat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class MobileFormat
+    {
+
+        public static string MobileNumber = "^(91)[6-9]{1}[0-9]{9}$";
+        
+        public bool Validate_MobileNumber(string Mobile_Number)
+        {
+            return Regex.IsMatch(MobileNumber, Mobile_Number);
+
+        }
+
+    }
+    
+}
