5a87eed61e6b0db981e7a2308462af47f29a1ce5
murgendrag <murgendra67@gmail.com>
2022-03-28 21:30:25 +0530

    Emailid added

---
+++ EmailId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+     class EmailId
+    {
+        public static string Email_id = "[a-z0-9!#$%&'*+/=?^_{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+        public bool Validate_Last_Name(string Emailid)
+        {
+            return Regex.IsMatch(Email_id, Emailid);
+
+        }
+    }
+}
